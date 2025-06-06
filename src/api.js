9999ae66191021a9dc8357e5cef836c135e7b7c6
siddhuparasa <siddhuparsa99@gmail.com>
2025-06-06 10:44:32 +0530

    new

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://jobcoach-backend-2.onrender.com/',
+baseURL: 'https://jobcoach-backend-2.onrender.com/',
+//baseURL: 'http://localhost:5000/', //local 
 });
 
 export default API;
